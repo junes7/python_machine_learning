2ecb4553b7ea5936f754290faa445c5dc83a077b
junes7 <2junes@naver.com>
2021-03-19 01:01:07 +0900

    study how to use Imputer class in scikit-learn

--- sklearn_dataset.py
+++ sklearn_dataset.py
@@ -11,6 +11,11 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1000)
 # print(X_train, X_test, Y_train, Y_test)
 
+import numpy as np
+X = np.random.uniform(0.0, 1.0, size=(10, 2))
+Y = np.random.choice(('Male', 'Female'), size=(10))
+print(X[0], Y[0])
+
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 yt = le.fit_transform(Y)
@@ -19,7 +24,7 @@ print(yt)
 from sklearn.preprocessing import LabelBinarizer
 lb = LabelBinarizer()
 Yb = lb.fit_transform(Y)
-print(lb.inverse_transfrom(Yb))
+# print(lb.inverse_transfrom(Yb))
 
 from sklearn.preprocessing import OneHotEncoder
 data = [
@@ -29,6 +34,19 @@ data = [
     [0, 12],
     [0, 15]
 ]
-oh = OneHotEncoder(categorical_features=[0])
-Y_oh = oh.fit_transform(data)
-print(Y_oh.todense())
+# oh = OneHotEncoder(categorical_features=[0])
+# Y_oh = oh.fit_transform(data)
+# print(Y_oh.todense())
+
+from sklearn.preprocessing import Imputer
+data = np.array([[1, np.nan, 2], [2, 3, np.nan], [-1, 4, 2]])
+imp = Imputer(strategy='mean')
+print(imp.fit_transform(data))
+imp = Imputer(strategy='median')
+print(imp.fit_transform(data))
+imp = Imputer(strategy='most_frequent')
+print(imp.fit_transform(data))
+
+
+
+
